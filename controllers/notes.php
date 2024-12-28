de9e97b2997bf5f565960f26465d2d4c122eb753
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-12-28 19:56:57 -0400

    refactor notes controller

--- controllers/notes.php
+++ controllers/notes.php
@@ -3,7 +3,7 @@
 $config = require('config.php');
 $db = new Database($config['database']);
 
-$notes = $db->query('select * from notes')->fetchAll();
+$notes = $db->query('select * from notes')->all();
 
 $title = 'My Notes';
 
