60d62810265ecd8acdcadf2b459489cd5daf6c19
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-12-28 19:56:42 -0400

    refactor note controller

--- controllers/note.php
+++ controllers/note.php
@@ -3,23 +3,20 @@
 $config = require('config.php');
 $db = new Database($config['database']);
 
+$title = 'Note';
+$currentUser = 1;
+
 $note = $db->query(
     query: 'select * from notes where id = :id',
     params: [
         'id' => $_GET['id'],
     ],
-)->fetch();
+)->findOrFail();
 
 if (! $note) {
     abort();
 }
 
-$currentUser = 1;
-
-if ($note['user_id'] !== $currentUser) {
-    abort(Response::Forbidden);
-}
-
-$title = 'Note';
+authorize($note['user_id'] === $currentUser);
 
 require 'views/note.view.php';
