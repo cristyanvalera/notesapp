bdf413116aab004e991960393a795c0f7623ba34
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-12-29 13:45:59 -0400

    remove function related to routes in function.php

--- functions.php
+++ functions.php
@@ -15,29 +15,3 @@ if (! function_exists('urlIs')) {
         return parse_url($_SERVER['REQUEST_URI'])['path'] === $value;
     }
 }
-
-if (! function_exists('abort')) {
-    function abort(Response $code = Response::PageNotFound): never {
-        http_response_code($code->value);
-
-        require "views/{$code->value}.php";
-
-        die();
-    }
-}
-
-if (! function_exists('authorize')) {
-    function authorize(bool $condition, Response $statusCode = Response::Forbidden): void {
-        if (! $condition) {
-            abort($statusCode);
-        }
-    }
-}
-
-if (! function_exists('routeToController')) {
-    function routeToController(string $uri, array $routes): void {
-        array_key_exists($uri, $routes)
-            ? require $routes[$uri]
-            : abort();
-    }
-}
