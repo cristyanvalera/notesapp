75471b880311f2ff871a6d29cc821e804b90e92f
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-12-29 14:38:15 -0400

    add simple validation logic

--- controllers/notes-create.php
+++ controllers/notes-create.php
@@ -1,13 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 $config = require('config.php');
 $db = new Database($config['database']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $result = $db->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)", [
-        'body' => $_POST['body'],
-        'user_id' => 1,
-    ]);
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body can not be than 1.000 characters';
+    }
+
+    if (empty($errors)) {
+        $db->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)", [
+            'body' => $_POST['body'],
+            'user_id' => 1,
+        ]);
+    }
 }
 
 $title = 'Create Note';
