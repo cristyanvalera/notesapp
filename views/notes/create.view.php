6ccc52b86ca6045b4a790422bf8ac7d2f82b6b40
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-12-30 16:00:08 -0400

    update partial file inclusion paths to use base_path

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -1,11 +1,11 @@
-<?php require('views/partials/head.php') ?>
-<?php require('views/partials/nav.php') ?>
-<?php require('views/partials/banner.php') ?>
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+<?php require base_path('views/partials/banner.php') ?>
 
 <main>
     <div class="mx-auto max-w-2xl px-4 py-6 sm:px-6 lg:px-8">
-        <?php require('views/partials/form.php') ?>
+        <?php require base_path('views/partials/form.php') ?>
     </div>
 </main>
 
-<?php require('views/partials/footer.php') ?>
+<?php require base_path('views/partials/footer.php') ?>
