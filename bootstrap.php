3d5f3a6e75ee5873b1a8452cac185a7a755c7b00
Cristyan Valera <cristyan.valera.81@gmail.com>
2025-01-03 21:05:33 -0400

    setContainer in bootstrap file

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+use Core\{App, Container, Database};
+
+App::setContainer(new Container())::bind(Database::class, function () {
+    $config = require base_path('config.php');
+
+    return new Database($config['database']);
+});
