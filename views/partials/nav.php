c9583017f8b8473aed17340c9bb6fffaead27fb2
Cristyan Valera <cristyan.valera.81@gmail.com>
2025-01-06 16:56:11 -0400

    feat: update banner title and conditionally display notes link in navigation

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -25,7 +25,9 @@
                     <div class="flex space-x-4">
                         <a href="/" class="<?= urlIs("/") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Home</a>
                         <a href="/about" class="<?= urlIs("/about") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">About</a>
-                        <a href="/notes" class="<?= urlIs("/notes") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Notes</a>
+                        <?php if ($_SESSION['user'] ?? false) : ?>
+                            <a href="/notes" class="<?= urlIs("/notes") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Notes</a>
+                        <?php endif; ?>
                         <a href="/contact" class="<?= urlIs("/contact") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Contact</a>
                     </div>
                 </div>
