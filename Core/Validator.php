883aa36d6d3965c3a6a39c3220903326edf1d1f8
Cristyan Valera <cristyan.valera.81@gmail.com>
2025-01-05 20:38:09 -0400

    added email and password validation methods

--- Core/Validator.php
+++ Core/Validator.php
@@ -12,4 +12,44 @@ class Validator
 
         return strlen($value) >= $min && strlen($value) <= $max;
     }
+
+    public static function email(string $value): bool
+    {
+        if (! filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function password(string $password, int $min = 7, int|float $max = 255): bool
+    {
+        return self::text($password, min: $min, max: $max);
+
+        // if (! self::text($password, min: 7, max: 255)) {
+        //     return false;
+        // }
+
+        // [$hasLower, $hasUpper, $hasNumber, $hasSymbol] = [false, false, false, false];
+
+        // foreach (str_split($password) as $char) {
+        //     match (true) {
+        //         ctype_lower($char) => $hasLower = true,
+        //         ctype_upper($char) => $hasUpper = true,
+        //         ctype_digit($char) => $hasNumber = true,
+        //         ! ctype_alnum($char) => $hasSymbol = true,
+        //     };
+
+        //     if ($hasLower && $hasUpper && $hasNumber && $hasSymbol) {
+        //         return true;
+        //     }
+        // }
+
+        // return $hasLower && $hasUpper && $hasNumber && $hasSymbol;
+    }
+
+    // public static function password(string $value): bool
+    // {
+    //     return preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d])[\s\S]{8,}$/', $value);
+    // }
 }
