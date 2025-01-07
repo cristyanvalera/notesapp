75f0653fcbfc206912ff446823440a3c3d9ebec8
Cristyan Valera <cristyan.valera.81@gmail.com>
2025-01-06 20:34:24 -0400

    login and logout

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -25,33 +25,48 @@
                     <div class="flex space-x-4">
                         <a href="/" class="<?= urlIs("/") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Home</a>
                         <a href="/about" class="<?= urlIs("/about") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">About</a>
+
                         <?php if ($_SESSION['user'] ?? false) : ?>
                             <a href="/notes" class="<?= urlIs("/notes") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Notes</a>
                         <?php endif; ?>
+
                         <a href="/contact" class="<?= urlIs("/contact") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Contact</a>
                     </div>
                 </div>
             </div>
 
             <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    <span class="absolute -inset-1.5"></span>
-                    <span class="sr-only">View notifications</span>
+                <?php if ($_SESSION['user'] ?? false): ?>
+                    <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span class="absolute -inset-1.5"></span>
+                        <span class="sr-only">View notifications</span>
 
-                    <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-                    </svg>
-                </button>
+                        <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                        </svg>
+                    </button>
+                <?php endif; ?>
 
                 <div class="relative ml-3">
                     <div>
                         <?php if ($_SESSION['user'] ?? false) : ?>
                             <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                         <?php else : ?>
-                            <a href="/register" class="text-white">Register</a>
+                            <a href="/register" class="<?= urlIs("/register") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Register</a>
+                            <a href="/login" class="<?= urlIs("/login") ? "bg-gray-900 text-white" : "text-gray-300" ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Log In</a>
                         <?php endif; ?>
                     </div>
                 </div>
+
+                <?php if ($_SESSION['user'] ?? false) : ?>
+                    <div class="ml-3">
+                        <form action="/session" method="post">
+                            <input type="hidden" name="_method" value="delete">
+
+                            <button type="submit" class="bg-gray-800 text-gray-300 rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Log Out</button>
+                        </form>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
