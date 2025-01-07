3aa233bd32e38a621be66d94f418fb76ce511575
Cristyan Valera <cristyan.valera.81@gmail.com>
2025-01-06 20:34:44 -0400

    wip

--- views/index.view.php
+++ views/index.view.php
@@ -4,8 +4,7 @@
 
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
-        Hello, you are in the Home page.
+        Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>!, you are in the Home page.
     </div>
 </main>
 
